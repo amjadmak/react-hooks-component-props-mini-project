6b94dcade293e68ad6eabd521812941b3401b23c
Amjad <amjadmaqsuma@gmail.com>
2022-06-06 23:49:37 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import About from "./about";
+import ArticleList from "./articleList";
+import Header from "./header";
 import blogData from "../data/blog";
 
-console.log(blogData);
-
-function App() {
+const App = () =>{
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
